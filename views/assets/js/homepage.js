67bb47180a33d12a84368c74d3853089fc288ccd
nateyeazel <nateyeazel@gmail.com>
2014-07-27 07:32:32 -0700

    Impressive changes to homepage

--- views/assets/js/homepage.js
+++ views/assets/js/homepage.js
@@ -3,8 +3,8 @@ var pageAssemble = function(contentArray) {
 		var contentTable = $('#main-content');
 		switch(element.type) {
 			case 'instagram':
-			contentTable.append('<tr><td><img src="' + element.imageSource +
-				'" class="col-md-8 col-md-offset-2 img-responsive " alt="Responsive image"></td></tr>');
+			contentTable.append('<tr><td class="col-md-8 col-md-offset-2" ><img src="' + element.imageSource +
+				'" class="img-responsive " alt="Responsive image"></td></tr>');
 			break;
 			case 'tweet':
 
